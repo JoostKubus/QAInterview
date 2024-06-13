18206ea0742a528da23b01d2c3c62e06ed229d09
joost renders <jrenders@kubusinfo.nl>
2024-06-13 11:29:08 +0200

    Example testfile

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,50 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('login screen', async ({ page }) => {
+  await page.goto('https://sandbox.bimcollab.com/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Expect login screen to show up.
+  await expect(page.locator('#panelLogoLink')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+  await expect(page.getByPlaceholder('Email address')).toBeVisible();
+  await expect(page.getByPlaceholder('Password')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Forgot your password?' })).toBeVisible();
+
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
+test('login', async ({ page }) => {
+  await page.goto('https://sandbox.bimcollab.com/');
+await page.getByPlaceholder('Email address').click();
+await page.getByPlaceholder('Email address').fill('');
+await page.getByPlaceholder('Email address').press('Tab');
+await page.getByPlaceholder('Password').fill('');
+await page.getByPlaceholder('Password').press('Enter');
+// Expects a valid login 
+await expect(page.getByRole('img', { name: 'Q A' })).toBeVisible();
+//verify role 
+await expect(page.locator('#teamMemberRole').getByText('Editor')).toBeVisible();
+});
+test('issue management', async ({ page }) => {
+  await page.goto('https://sandbox.bimcollab.com/');
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+await page.getByPlaceholder('Email address').click();
+await page.getByPlaceholder('Email address').fill('');
+await page.getByPlaceholder('Email address').press('Tab');
+await page.getByPlaceholder('Password').fill('');
+await page.getByPlaceholder('Password').press('Enter');
+//manage an issue
+await page.getByRole('link', { name: 'QA' }).click();
+await page.getByRole('link', { name: 'Issues' }).click();
+await page.waitForLoadState('networkidle');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+await expect(page.getByRole('gridcell', { name: 'Active' })).toBeVisible();
+
+await page.locator('#divSubmenuIcon').hover();
+await page.locator('#HyperLinkSubMenuResolveAndClose').click();
+await expect(page.getByRole('gridcell', { name: 'Closed' })).toBeVisible();
+
+await page.locator('#divSubmenuIcon').hover();
+await page.locator('#HyperLinkSubMenuReopen').click();
+await expect(page.getByRole('gridcell', { name: 'Active' })).toBeVisible();
 });
